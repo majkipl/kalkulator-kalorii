ed8458135ca3627d832d61e6bc5ca2c9a2d1ab5c
Rafał Majkowski <majkipl@gmail.com>
2025-08-28 18:09:16 +0200

    refaktoryzacja dla firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,13 +6,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Konfiguracja Twojej aplikacji Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyCWLLc_kTrKkVIYymPRHhWehoa-P7mw4wY",
-    authDomain: "cat-calculator-dd26f.firebaseapp.com",
-    projectId: "cat-calculator-dd26f",
-    storageBucket: "cat-calculator-dd26f.firebasestorage.app",
-    messagingSenderId: "869938257249",
-    appId: "1:869938257249:web:3c0def8fb96f034e277920",
-    measurementId: "G-8PJVDQNXSZ"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Inicjalizacja Firebase
