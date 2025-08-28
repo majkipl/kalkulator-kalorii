c5e53f56c8b37793fda9d56fc62ce018809ce069
Rafał Majkowski <majkipl@gmail.com>
2025-08-28 20:00:43 +0200

    Centralizacja Stałych i "Magicznych" Wartości

--- src/components/CatProfileForm.js
+++ src/components/CatProfileForm.js
@@ -8,6 +8,8 @@ import {LucideSave, LucideTrash2} from 'lucide-react';
 import {useAppContext} from '../context/AppContext';
 import {formStyles, getCustomSelectStyles, typographyStyles} from '../utils/formStyles';
 
+import { breedOptions, activityLevelOptions, physiologicalStateOptions, chronicDiseaseOptions } from '../config/options';
+
 const CatProfileForm = ({cat, onSave, onCancel, onDeleteRequest}) => {
     // Pobieramy 'theme' bezpośrednio z kontekstu
     const {theme, isDark} = useAppContext();
@@ -27,37 +29,10 @@ const CatProfileForm = ({cat, onSave, onCancel, onDeleteRequest}) => {
     });
 
     // Opcje dla pól typu Select
-    const breedOptions = [{value: 'mieszany', label: 'Mieszany / Inny'}, {
-        value: 'europejski',
-        label: 'Europejski krótkowłosy'
-    }, {value: 'brytyjski', label: 'Brytyjski krótkowłosy'}, {
-        value: 'maine_coon',
-        label: 'Maine Coon'
-    }, {value: 'ragdoll', label: 'Ragdoll'}, {value: 'syberyjski', label: 'Syberyjski'}, {
-        value: 'bengalski',
-        label: 'Bengalski'
-    }, {value: 'sfinks', label: 'Sfinks'}];
-    const activityLevelOptions = [{value: 'niski', label: 'Niski (kot niewychodzący)'}, {
-        value: 'umiarkowany',
-        label: 'Umiarkowany (kot wychodzący)'
-    }, {value: 'wysoki', label: 'Wysoki (bardzo aktywny)'}];
-    const physiologicalStateOptions = [{value: 'normalny', label: 'Normalny'}, {
-        value: 'ciaza',
-        label: 'Ciąża'
-    }, {value: 'laktacja', label: 'Laktacja'}, {value: 'rekonwalescencja', label: 'Rekonwalescencja'}];
-    const chronicDiseaseOptions = [{value: 'brak', label: 'Brak'}, {
-        value: 'nadczynnosc_tarczycy',
-        label: 'Nadczynność tarczycy'
-    }, {value: 'choroba_nerek', label: 'Przewlekła choroba nerek'}, {
-        value: 'cukrzyca',
-        label: 'Cukrzyca'
-    }, {value: 'choroby_serca', label: 'Choroby serca'}, {
-        value: 'choroby_drog_moczowych',
-        label: 'Choroby dolnych dróg moczowych (FLUTD)'
-    }, {value: 'zapalenie_trzustki', label: 'Zapalenie trzustki'}, {
-        value: 'nieswoiste_zapalenie_jelit',
-        label: 'Nieswoiste zapalenie jelit (IBD)'
-    }];
+
+
+
+
 
     // Logika stylów dla react-select na podstawie motywu
     const customSelectStyles = getCustomSelectStyles(isDark);
---
+++ src/config/options.js
@@ -0,0 +1,36 @@
+// /src/config/options.js
+
+export const breedOptions = [
+    {value: 'mieszany', label: 'Mieszany / Inny'},
+    {value: 'europejski', label: 'Europejski krótkowłosy'},
+    {value: 'brytyjski', label: 'Brytyjski krótkowłosy'},
+    {value: 'maine_coon', label: 'Maine Coon'},
+    {value: 'ragdoll', label: 'Ragdoll'},
+    {value: 'syberyjski', label: 'Syberyjski'},
+    {value: 'bengalski', label: 'Bengalski'},
+    {value: 'sfinks', label: 'Sfinks'}
+];
+
+export const activityLevelOptions = [
+    {value: 'niski', label: 'Niski (kot niewychodzący)'},
+    {value: 'umiarkowany', label: 'Umiarkowany (kot wychodzący)'},
+    {value: 'wysoki', label: 'Wysoki (bardzo aktywny)'}
+];
+
+export const physiologicalStateOptions = [
+    {value: 'normalny', label: 'Normalny'},
+    {value: 'ciaza', label: 'Ciąża'},
+    {value: 'laktacja', label: 'Laktacja'},
+    {value: 'rekonwalescencja', label: 'Rekonwalescencja'}
+];
+
+export const chronicDiseaseOptions = [
+    {value: 'brak', label: 'Brak'},
+    {value: 'nadczynnosc_tarczycy', label: 'Nadczynność tarczycy'},
+    {value: 'choroba_nerek', label: 'Przewlekła choroba nerek'},
+    {value: 'cukrzyca', label: 'Cukrzyca'},
+    {value: 'choroby_serca', label: 'Choroby serca'},
+    {value: 'choroby_drog_moczowych', label: 'Choroby dolnych dróg moczowych (FLUTD)'},
+    {value: 'zapalenie_trzustki', label: 'Zapalenie trzustki'},
+    {value: 'nieswoiste_zapalenie_jelit', label: 'Nieswoiste zapalenie jelit (IBD)'}
+];
